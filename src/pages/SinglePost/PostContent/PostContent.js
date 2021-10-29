3dd8a95062f8c917f70d273b2f5f93d02ca3ce7c
Hossam <56669856+HOSS11H@users.noreply.github.com>
2021-10-29 15:49:46 +0200

    Tabs Configure

--- src/pages/SinglePost/PostContent/PostContent.js
+++ src/pages/SinglePost/PostContent/PostContent.js
@@ -3,12 +3,18 @@ import Resala from '../../../images/logos/resala.jpg';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import TabPanel from '../../../components/UI/TabPanel/TabPanel';
+import { useState } from 'react';
+import ThemeContext from '../../../store/theme-context';
+import { useContext } from 'react';
 
 const ContentWrapper = styled.div`
-
+    margin-bottom: 10px;
 `
 const PostHead = styled.div`
-
+    margin-bottom: 5px;
 `
 const PostImage = styled.img`
     display: block;
@@ -74,30 +80,42 @@ const PostSocialLink = styled.a`
     }
 `
 const InvestButton = styled.button`
-    font-size:20px;
-    width: 150px;
-    font-weight: 700;
-    padding: 0 25px;
-    height: 50px;
+    width: 120px;
+    padding: 0 15px;
+    height: 30px;
     display: inline-flex;
     justify-content: center;
     align-items: center;
     background-color: ${ ( { theme } ) => theme.vars.primary  };
-    color: ${ ( { theme } ) => theme.palette.mode === 'dark' ? theme.vars.white : theme.vars.black };
+    color: ${ ( { theme } ) => theme.vars.white };
+    font-size: 14px;
+    font-weight: 500;
     border:0;
     outline: none;
     cursor: pointer;
-    border-radius: 8px;
+    border-radius: 12px;
     transition: 0.3s ease-in-out;
-    &:hover {
-        color: ${ ( { theme } ) => theme.palette.mode === 'dark' ? theme.vars.black : theme.vars.white };
-    }
+    text-transform: capitalize;
 `
-
+// Function To Handle Tabs Index
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
 
 
 
 const PostContent = ( props ) => {
+
+    const [ currentTab , setCurrentTab ] = useState(0);
+
+    const themeCtx = useContext(ThemeContext);
+
+    const currentTabHandler = (event, newValue) => {
+        setCurrentTab(newValue);
+    };
 
     const loadedTags = ['loan', 'industry'].map( ( tag , index ) => {
         return <Tag key={index}>{tag}</Tag>
@@ -125,6 +143,32 @@ const PostContent = ( props ) => {
                 </PostSocials>
                 <InvestButton>invest now</InvestButton>
             </PostHead>
+            <Tabs value={currentTab} onChange={currentTabHandler} aria-label="basic tabs example" 
+                sx= { { borderBottom: `1px solid ${themeCtx.theme.palette.mode === 'dark' ? themeCtx.theme.vars.white : themeCtx.theme.vars.black}`, 
+                '& .MuiTabs-indicator': { backgroundColor: `${themeCtx.theme.palette.mode === 'dark' ? themeCtx.theme.vars.white : themeCtx.theme.vars.black}` },  }  }>
+                <Tab label="pitch" {...a11yProps(0)} 
+                    sx={ { 
+                        padding: '12px 0 0px', marginRight: '55px', minWidth: 'unset', fontSize: '18px', 
+                        fontWeight: 800, textTransform: 'capitalize',
+                        color: `${themeCtx.theme.palette.mode === 'dark' ? themeCtx.theme.vars.white : themeCtx.theme.vars.black}` , 
+                        '&.Mui-selected': { color: `${themeCtx.theme.palette.mode === 'dark' ? themeCtx.theme.vars.white : themeCtx.theme.vars.black}` } 
+                    } } 
+                />
+                <Tab label="company" {...a11yProps(1)} 
+                    sx={ { 
+                        padding: '12px 0 0px', marginRight: '55px', minWidth: 'unset', fontSize: '18px', 
+                        fontWeight: 800, textTransform: 'capitalize',
+                        color: `${themeCtx.theme.palette.mode === 'dark' ? themeCtx.theme.vars.white : themeCtx.theme.vars.black}` , 
+                        '&.Mui-selected': { color: `${themeCtx.theme.palette.mode === 'dark' ? themeCtx.theme.vars.white : themeCtx.theme.vars.black}` } 
+                    } } 
+                />
+            </Tabs>
+            <TabPanel value={currentTab} index={0}>
+                Item One
+            </TabPanel>
+            <TabPanel value={currentTab} index={1}>
+                Item Two
+            </TabPanel>
         </ContentWrapper>
     );
 }
